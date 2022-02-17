a42407c1b3053241fcfba92692a873b1c8da008e
Silas Lenihan <sjl58@duke.edu>
2022-02-17 15:32:45 -0500

    Refactored Terra RPC_URL environment variables

--- packages/sources/terra-view-function/src/config/index.ts
+++ packages/sources/terra-view-function/src/config/index.ts
@@ -11,6 +11,7 @@ export type ChainId = typeof SUPPORTED_CHAIN_IDS[number]
 export const ENV_DEFAULT_CHAIN_ID = 'DEFAULT_CHAIN_ID'
 export const DEFAULT_CHAIN_ID = 'columbus-5'
 
+export const ENV_LCD_URL = 'LCD_URL'
 export const ENV_RPC_URL = 'RPC_URL'
 
 export type Config = BaseConfig & {
@@ -35,13 +36,19 @@ const buildRpcUrlMapping = () => {
   for (const chainId of SUPPORTED_CHAIN_IDS) {
     // Underscore-ize and capitalize to format for environment variables
     const prefix = chainId.replace('-', '_').toUpperCase()
-    const envVar = util.getEnv(ENV_RPC_URL, prefix)
+    const envVar = util.getEnv(ENV_LCD_URL, prefix)
+    const legacyEnvVar = util.getEnv(ENV_RPC_URL, prefix)
     if (envVar) {
       output[chainId] = envVar
       hasAtLeastOneURL = true
+    } else if (legacyEnvVar) {
+      console.log(
+        'The RPC_URL environment variable is being deprecated, please use an LCD_URL instead. See README for more information.',
+      )
+      output[chainId] = legacyEnvVar
+      hasAtLeastOneURL = true
     }
   }
-
   if (!hasAtLeastOneURL) throw new Error('At least one RPC URL must be defined')
 
   return output
